a7fb63fd83510018921332ed7d7745f00b5bf120
David Araiz <davidaraizsaez@gmail.com>
2026-09-15 19:05:30 +0200

    prueba

--- VS  CODE/JAVASCRIPT/prueba.js
+++ VS  CODE/JAVASCRIPT/prueba.js
@@ -1,2 +1,12 @@
 console.log("Hello, World!");
+console.log("Prueba de sincronización");
 
+function saludar(nombre) {
+  return `Hola, ${nombre}!`;
+}
+
+console.log(saludar("David"));
+
+const numeros = [1, 2, 3, 4, 5];
+const suma = numeros.reduce((total, n) => total + n, 0);
+console.log("La suma es:", suma);
